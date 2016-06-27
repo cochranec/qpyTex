7328f9fef0854f7ae0d50bf1a5ed83d656e0a9ec
cochranec <cochranec@gmail.com>
2016-06-27 17:09:06 -0400

    Add initial files to repository

---
+++ pyTextFits_Iterate_TMS.py
@@ -0,0 +1,202 @@
+#import threading
+from multiprocessing import Process, Queue
+import os
+import glob
+from pyTexture import guessDSpacing, getPeakCens, gauss, getHKL, fakePseudoVoigt, textResample
+from itertools import count
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+np.seterr(divide='ignore')
+
+iPlot = False
+
+num_X = 2048
+num_Y = 2048
+num = 2000
+areaScale = np.sqrt(np.log(2)/np.pi)
+arcRad = 1024 * 1.42
+rhoStart, rhoEnd = -180, 180
+rhoN = 360
+outDir = '/home/chris/PyText/Data2/'
+
+import logging
+logger = logging.getLogger('myapp')
+hdlr = logging.FileHandler(outDir + '/pyText.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.WARNING)
+
+
+def textureFitsFile(inFilename, pF, wavelength, cake=True, doPlot=False):
+    # ptList = []
+    xLi = []
+    yLi = []
+    areaLi = []
+    angleList = []
+    cLi = []
+    pkLi = []
+    print 'Reading ' + inFilename
+    with open(inFilename, mode='rb') as fileobj:
+        rawData = np.fromfile(fileobj, np.float32, num_X * num_Y).astype('float32')
+    z = rawData.reshape((num_X, num_Y));
+
+    #peakCens = getPeakCens('hcp',[3.23433391, 5.163978], 2)
+    peakCens = np.array([2.78126, 2.4591, 2.585,1.90, 1.60544, 1.2384])
+    #peakHKL  = getHKL('hcp',2)
+    peakHKL = [(1,0,0),(1,0,1),(0,0,2),(1,0,2),(1,1,0),(2,0,2)]
+    peakWids = np.repeat(0.01,len(peakCens))
+    dMax = np.max(peakCens) + 0.5
+    x1, y1 = pF[0], pF[1]
+
+    if cake:
+        xd, yd   = pF[0] - np.arange(2048), pF[1] - np.arange(2048)
+        xv, yv = np.meshgrid(xd, yd)
+        psi = np.degrees(np.arctan2(-np.sin(pF[3])*xv + np.cos(pF[2]) * yv, np.cos(pF[3]) * (np.cos(pF[2]) * xv + np.sin(pF[2]) * yv)))
+        arcWid = np.abs(rhoStart - rhoEnd) / rhoN / 2.
+    for angle in np.linspace(rhoStart, rhoEnd, rhoN):
+        x2, y2 = x1 + arcRad * np.cos(np.radians(angle)), y1 + arcRad * np.sin(np.radians(angle))
+        if cake:
+            cond1 = (psi > angle - arcWid) & (psi < angle + arcWid)
+            cakeMat = np.where(cond1)
+            x, y = xv[cakeMat]+pF[0], yv[cakeMat]+pF[1]
+        else:
+            x, y = np.linspace(x1, x2, num), np.linspace(y1, y2, num)
+        dSpac = guessDSpacing(pF, x, y, wavelength)
+        kKeys = np.where((dSpac < dMax) & (0 < x) & (x < num_X) & (0 < y) & (y < num_Y))
+        x = x[kKeys]
+        y = y[kKeys]
+        dSpac=dSpac[kKeys]
+        if len(dSpac) is 0:
+            print 'No measurement for angle of %d.' % angle
+            pass
+        else:
+            if cake:
+                binedges = np.linspace(np.min(dSpac),np.max(dSpac),512)
+                dg = np.digitize(dSpac,binedges)
+                ind    = np.argsort(dg)
+                splits = np.where(np.diff(dg[ind]))[0]
+                sliceInt = np.split(z[cakeMat][kKeys][ind],splits)[1:];
+                zi= np.array([x.mean() for x in sliceInt])
+                dSpac = np.array(binedges[1:])
+            else:
+                zi = textResample(z,x,y)
+            if doPlot:
+                _, ax = plt.subplots(1,1)
+                ax.plot(dSpac[:np.size(zi)],zi,'k.-')
+            '''
+            BGmm1=np.argmin(np.abs(dSpac-(2.17+0.01*7)))
+            BGmm2=np.argmin(np.abs(dSpac-(2.17-0.01*7)))
+            BGmmRange = range(np.max((np.min((BGmm1,BGmm2)),0)),np.min((np.max((BGmm1,BGmm2)),len(zi))))
+            BGArea = np.sum(zi[BGmmRange])
+            '''
+
+            for c in [xx for xx in peakCens if xx > np.min(dSpac)]:
+                #mmX = np.argmin(np.abs(dSpac-c))
+                #print c
+                mm1=np.argmin(np.abs(dSpac-(c+peakWids[peakCens==c][0]*7)))
+                mm2=np.argmin(np.abs(dSpac-(c-peakWids[peakCens==c][0]*7)))
+                mmRange = np.array(range(np.max((np.min((mm1,mm2)),0)),np.min((np.max((mm1,mm2)),len(zi)))))
+                #if np.any(zi[mmRange] <= 0) or np.all(c > (dSpac[mmRange])):
+                if False:
+                    pass
+                else:
+                    '''
+                    p0 = [np.max(zi[mmRange]) - np.min(zi[mmRange]),dSpac[mmX],peakWids[peakCens==c],np.min(zi[mmRange])]
+                    p0 = [np.max(zi[mmRange]) - np.min(zi[mmRange]),dSpac[mm2+np.argmax(zi[mmRange])],peakWids[peakCens==c],np.min(zi[mmRange])]
+                    try:
+                        coeff, _ = curve_fit(gauss, dSpac[mmRange], zi[mmRange], p0,xtol=1e-2)
+                    except:
+                        coeff = np.array([0, c, 1e-3, 0])
+                    '''
+                    coeff    = np.array([np.sum(zi[mmRange])/np.min(zi[mmRange]),c,1e-3,0])
+                    coeff[0] = np.abs(coeff[0])
+                    coeff[2] = np.abs(coeff[2])
+
+                    mRan = zi[mmRange].argsort()[:5]
+                    bx = np.hstack(([mmRange[0],mmRange[-1]], mmRange[mRan]))
+                    by = np.hstack(([zi[mmRange[0]],zi[mmRange[-1]]],zi[mmRange][mRan]))
+                    bkfit = np.polyfit(bx,by,2);
+                    newArea = np.sum(zi[mmRange]) - np.sum(np.polyval(bkfit,mmRange))
+
+                    p0 = [np.max(zi[mmRange]) - np.min(zi[mmRange]),dSpac[mm2+np.argmax(zi[mmRange])],peakWids[peakCens==c],np.min(zi[mmRange])]
+                    try:
+                        coeff, _ = curve_fit(fakePseudoVoigt, dSpac[mmRange], zi[mmRange]-np.polyval(bkfit,mmRange), p0,xtol=1e-2)
+                    except:
+                        coeff = np.array([0, c, 1e-3, 0])
+                                       
+                    if doPlot:
+                        resca = np.linspace(dSpac[mmRange[0]],dSpac[mmRange[-1]],500)
+                        resca2 = np.linspace(mmRange[0],mmRange[-1],500)
+                        #pk_fit = gauss(resca, *coeff)
+                        #ax.plot(dSpac[mmRange],zi[mmRange],'.')
+                        #ax.axvline(dSpac[mmRange[0]])
+                        #ax.axvline(dSpac[mmRange[-1]])
+                        ax.plot(dSpac[bx],by,'ro')
+                        ax.plot(resca,fakePseudoVoigt(resca,*coeff)+np.polyval(bkfit,resca2),'-',lw=2)
+
+                    if coeff[2] > 0.1:
+                        print coeff
+                        #ax.plot(resca,gauss(resca,*p0),'k--')
+                        pass
+                    else:
+                        zi[mmRange] = zi[mmRange] - fakePseudoVoigt(dSpac[mmRange],*coeff)
+                        #if doPlot: ax.plot(resca,pk_fit,'-')
+                        #areaLi = np.append(areaLi, coeff[0] * coeff[2] * areaScale)
+                        areaLi = np.append(areaLi, newArea)
+                        if not cake:
+                            xLi = np.append(xLi, np.interp(coeff[1],range(np.size(kKeys)),x))
+                            yLi = np.append(yLi, np.interp(coeff[1],range(np.size(kKeys)),y))
+                        angleList = np.append(angleList, angle)
+                        cLi = np.append(cLi, coeff[1])
+                        pkLi= np.append(pkLi,np.where(peakCens==c)[0][0])
+                        if (c == 2.585):
+                            pass
+                        else:
+                            np.place(peakCens,peakCens == c,coeff[1])
+                        #np.place(peakWids,peakCens == c,coeff[2])
+        if doPlot:
+            ax.set_ylim(0,650)
+            ax.set_xlim(1.2,3)
+            print '%.2f' % angle
+            ax.plot(dSpac[:np.size(zi)],zi,'r.')
+            plt.show()
+    theta = 90 - 0 * cLi#- np.degrees(np.arcsin(wavelength / (2 * cLi))/2)
+    runNum = inFilename.split('_')[-1].split('.')[0]
+    try:
+        os.makedirs(outDir + runNum)
+    except:
+        pass
+    outputName = inFilename.split('/')[-1].split('.')[0]
+    if cake:
+        outputName += 'cake'
+    for K, n in zip(peakHKL,range(len(peakHKL))):
+        nKeys = np.where(pkLi == n)
+        if len(nKeys[0]) > 5:
+            #print '**\n%d%d%d has ' % K + str(len(nKeys[0])) + ' fits.'
+            outFile = outDir + runNum + '/' + outputName + '.%d%d%d' % K
+            #print 'Output to ' + outFile
+            M = np.vstack((theta[nKeys], angleList[nKeys], areaLi[nKeys]))
+            np.savetxt(outFile,M.T,fmt='%.6f',delimiter=' ')
+    print 'Files output to ' + outputName
+
+
+lam = 0.1441
+ceriaDat = np.array([1.01141128e+03,   1.02430730e+03,  -1.70246315e+03,  -3.39086205e-02, 1.60051457e+00])
+fList = glob.glob('/home/chris/Tubes/ge/H737*sum') + glob.glob('/home/chris/Tubes/ge/CC009Real*sum') + glob.glob('/home/chris/Tubes/ge/RX213*sum') + glob.glob('/home/chris/Tubes/ge/GE4Tube*sum')
+a = (ceriaDat,lam)
+
+for f, n in zip(fList,count()):
+    oName = outDir + f.split('_')[-1].split('.')[0] + '/'
+
+#    if (~os.path.exists(oName)):
+    try:
+        textureFitsFile(f, *a, doPlot=iPlot)
+    except:
+        st = 'File error: ' + f.split('_')[-1].split('.')[0]
+        logger.error(st)
+#    else:
+#        pass
+    print '%d of %d files complete.' % (n+1, len(fList))
---
+++ pyTexture.py
@@ -0,0 +1,187 @@
+'''
+Created on Nov 21, 2014
+
+@author: chris
+'''
+
+from itertools import combinations_with_replacement, combinations, product, repeat,chain
+from warnings import warn
+from numpy import sin, cos, sqrt, arctan2, exp
+from scipy.optimize import leastsq, curve_fit
+import numpy as np
+import threading
+from Queue import Queue
+import matplotlib.pyplot as plt
+
+def isBCC(x):
+    if sum(x) == 0:
+        return 0
+    else:
+        return not (sum(x) % 2) == 1
+
+def isFCC(x):
+    if sum(x) == 0:
+        return 0
+    else:
+        return not ((x[0] + x[1]) % 2) and not ((x[1]+x[2]) % 2)
+
+def isHCP(x):
+    if sum(x) == 0:
+        return 0
+    else:
+        return not (x[2] % 2) or (x[0] + 2*x[1] % 3)
+
+def getHKL(sys, n=3):
+    if sys.lower() == "bcc":
+        fxn = isBCC
+    elif sys.lower() =="fcc":
+        fxn = isFCC
+    elif sys.lower() =="hcp":
+        fxn = isHCP
+        y = [[x[0][1], x[0][0], x[1]] for x in product(combinations_with_replacement(range(n+1),2),range(n+1))]
+        return [tuple(x) for x in y if fxn(x)]
+    else:
+        warn("No crystal system given.", SyntaxWarning)
+        return []
+    return [x for x in combinations_with_replacement(range(n+1), 3) if fxn(x)]
+
+def getPeakCens(sys, a, n=3):
+    hkl = getHKL(sys, n)
+    squares = []
+    if 'hcp' in sys.lower():
+        for x in hkl:
+            squares.append(4/3. * (x[0]**2 + x[0] * x[1] + x[1]**2)/a[0]**2 + x[2]**2/a[1]**2)
+    else:
+        for x in hkl:
+            squares.append((x[0]**2 + x[1]**2 + x[2]**2)/a)
+    return [1 / sqrt(x) for x in squares]
+
+def guessDSpacing(p, xv, yv, wavelength):
+    [xcg,ycg,betag,phig,dg] = p
+    pxC = 200e-6
+    xd = (xv - xcg) * pxC
+    yd = (yv - ycg) * pxC
+    TA = cos(phig)**2 *  ( cos(betag) * xd + sin(betag) * yd)**2
+    TB =                 (-sin(betag) * xd + cos(betag) * yd)**2
+    TC = (dg + sin(phig)*( cos(betag) * xd + sin(betag) * yd))**2
+    R = sqrt(TA + TB)
+    D = sqrt(TC)
+    delR = R * (1 - D / sqrt(TA + TB + TC))
+    delD = D * (1 - D / sqrt(TA + TB + TC))
+    trueR = R - delR
+    trueD = D - delD
+    TT = arctan2(trueR, trueD)
+    return wavelength / TT
+
+def gauss(x, *p):
+    A, mu, sigma, y0 = p
+    return A * exp(-(x - mu) ** 2 / (2.*sigma ** 2)) + y0
+
+def gaussDoublet(x, *p):
+    A1, mu1, sigma1, A2, mu2, sigma2, y0 = p
+    return gauss(x, A1, mu1, sigma1, y0) + gauss(x, A2, mu2, sigma2, 0)
+
+def fakePseudoVoigt(x, *p):
+    height, center, hwhm, y0 = p
+    shape=0.4
+    return height*((1-shape)*exp(-np.log(2)*(np.divide((x-center),hwhm))**2)+shape*1/(1+(np.divide(x-center,hwhm))**2)) +  y0
+
+def PseudoVoigt(x, *p):
+    height, center, hwhm, sha, y0 = p
+    shape = sin(sha)**2
+    return height*((1-shape)*exp(-np.log(2)*(np.divide((x-center),hwhm))**2)+shape*1/(1+(np.divide(x-center,hwhm))**2)) +  y0
+    #return A * exp(-(x - mu) ** 2 / (2.*sigma ** 2)) + y0
+
+def textResample(z, x, y):
+    x1 = x.astype('int')
+    x2 = np.clip(x1+1,0,2047)
+    y1 = y.astype('int')
+    y2 = np.clip(y1+1,0,2047)
+
+    EPSILON = (x1 + .5 - x) * -2
+    ETA     = (y1 + .5 - y) * -2
+    N1 = .25 * (1 - EPSILON) * (1 - ETA)
+    N2 = .25 * (1 + EPSILON) * (1 - ETA)
+    N3 = .25 * (1 + EPSILON) * (1 + ETA)
+    N4 = .25 * (1 - EPSILON) * (1 + ETA)
+
+    zo = z[x1,y1] * N1 + z[x2,y1] * N2 + z[x2,y2] * N3 + z[x1,y2] * N4
+    return zo
+
+# def runQueues(files, fxn, params):
+#     nThread = 7
+#     nFiles  = len(files)
+#     startTime = time.time()
+#
+#     q = Queue(maxsize=0)
+#
+#     for _ in range(nThread):
+#         worker = threading.Thread(target=feedQueue, args=(q, fxn, params,))
+#         worker.setDaemon(True)
+#         worker.start()
+#
+#     for fs in files:
+#         q.put(fs)
+#
+#     q.join()
+#
+#     print 'Done.  Average time per file: %.3fs' % ((time.time() - startTime)/nFiles)
+#
+# def feedQueue(q, fxn, param):
+#     while True:
+#         f = q.get()
+#         sizeOfQueue = q.qsize()
+#         fxn(f, *param)
+#         print str(sizeOfQueue) + ' files remain in queue.'
+#          nFilesComplete = nFile - sizeOfQueue
+#          timeSpent = time.time() - startT
+#          timeRemaining = numpy.round(timeSpent / nFilesComplete * sizeOfQueue)
+#          m, s = divmod(timeRemaining, 60)
+#          h, m = divmod(m, 60)
+#          print 'File ', nFilesComplete, '/', nFile, '-',
+#          print "%d:%02d:%02d to completion." % (h, m, s)
+#         q.task_done()
+
+#def guessDoublet(foo):
+
+if __name__ == '__main__':
+    bb=False
+    if bb:
+        p = [3.23, 5.15]
+        a = np.array(getPeakCens('hcp',p,2))
+        err = lambda p: [np.array(getPeakCens('hcp',p,2))[0] - 2.581989, np.array(getPeakCens('hcp',p,2))[2] - 2.462139]
+        print err(p)
+        x, cov = leastsq(err, p, args=())
+        print x
+    if ~bb:
+        xr = np.arange(-10,10,.1)
+        pCor= [1, 0, .3,
+               2, 1, .2,
+               0.1]
+        foo = gaussDoublet(xr, *pCor) + np.random.rand(len(xr))*.2
+
+        errfunc = lambda p, x, y: gauss(x, *p) - y
+
+        c1 = np.argmax(foo)
+        guess1=[1, xr[c1],.1,0]
+        f1 = gauss(xr, *guess1)
+
+        p1, _ = curve_fit(gauss, xr, foo, guess1,xtol=1e-8)
+
+        foo2 = -errfunc(p1,xr,foo)
+        c2 = np.argmax(foo2)
+        guess2=[1, xr[c2],.1,0]
+        f2 = gauss(xr, *guess2)
+        p2, _ = curve_fit(gauss, xr, foo2, guess2,xtol=1e-8)
+
+        f3 = gauss(xr, *p1) + gauss(xr, *p2)
+        fig, ax = plt.subplots(1,1)
+
+        ax.plot(xr, foo,'.')
+        ax.plot(xr, foo2,'.')
+        ax.plot(xr, f1+f2-guess1[-1],'g-', label='Initial Guess')
+        ax.plot(xr, f3,'r-', label='Fit')
+        ax.legend()
+        peakCen = [guess1[1], guess2[1]]
+        print peakCen
+        plt.show()
